5ff0cafebe3b4211f0c31ca656a4cfeeff8b033a
Peach-He <tao1.he@intel.com>
2021-05-25 09:13:07 +0800

    inference and scripts

--- tf2/WideAndDeep/scripts/inference.sh
+++ tf2/WideAndDeep/scripts/inference.sh
@@ -14,21 +14,6 @@ set -e
  export I_MPI_PIN_PROCESSOR_EXCLUDE_LIST="16,17,34,35,52,53,70,71"
 # export HOROVOD_CCL_CACHE=1
 
-# horovodrun -np 6 -H sr610:2,sr612:2,sr613:2 --start-timeout 300 --timeline-filename timeline.json \
-# --mpi-args="-genv OMP_NUM_THREADS=16 -genv CCL_WORKER_COUNT=2 -map-by socket" \
-# /root/sw/miniconda3/envs/wd2/bin/python main.py \
-#   --train_data_pattern /mnt/sdd/outbrain2/tfrecords/train/part* \
-#   --eval_data_pattern /mnt/sdd/outbrain2/tfrecords/eval/part* \
-#   --model_dir /mnt/nvm6/wd/checkpoints2 \
-#   --transformed_metadata_path /outbrain2/tfrecords \
-#   --cpu \
-#   --num_epochs 10
-
-# time mpirun -genv OMP_NUM_THREADS 16 -genv CCL_WORKER_COUNT 2 -map-by socket -f scripts/hostfile \
-# -n 2 bash /root/ht/ML/wd-code/tf2/WideAndDeep/scripts/launch.sh \
-# : -n 2 bash /root/ht/ML/wd-code/tf2/WideAndDeep/scripts/launch.sh \
-# : -n 2 bash /root/ht/ML/wd-code/tf2/WideAndDeep/scripts/launch.sh
-
 # time mpirun -genv OMP_NUM_THREADS=16 -map-by socket -n 2 -ppn 2 -hosts sr113 -print-rank-map \
 # -genv I_MPI_PIN_DOMAIN=socket -genv OMP_PROC_BIND=true -genv KMP_BLOCKTIME=1 \
 # /root/sw/miniconda3/envs/wd2/bin/python main.py \
@@ -41,16 +26,18 @@ set -e
 
 time mpirun -genv OMP_NUM_THREADS=16 -map-by socket -n 8 -ppn 2 -hosts sr113,sr610,sr612,sr613 -print-rank-map \
 -genv I_MPI_PIN_DOMAIN=socket -genv OMP_PROC_BIND=true -genv KMP_BLOCKTIME=1 -genv KMP_AFFINITY=granularity=fine,compact,1,0 \
--iface eth3 \
 /root/sw/miniconda3/envs/wd2/bin/python main.py \
   --train_data_pattern /mnt/sdd/outbrain2/tfrecords/train/part* \
   --eval_data_pattern /mnt/sdd/outbrain2/tfrecords/eval/part* \
   --model_dir /mnt/nvm6/wd/checkpoints2 \
   --transformed_metadata_path /outbrain2/tfrecords \
   --cpu \
-  --global_batch_size 524288 \
-  --eval_batch_size 524288 \
-  --num_epochs 50 \
   --deep_learning_rate 0.00048 \
   --linear_learning_rate 0.8 \
+  --eval_batch_size 1048576 \
+  --evaluate \
+  --use_checkpoint \
+  --benchmark \
+  --benchmark_warmup_steps 0 \
+  --benchmark_steps 1000 \
   --deep_hidden_units 1024 512 256
